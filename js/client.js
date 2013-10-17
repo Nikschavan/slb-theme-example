3335e8d6458bed475d2435764f8f8b0abaf7fd64
SM <sol@archetyped.com>
2013-10-17 09:07:51 -1000

    Add skeleton JS file

---
+++ js/client.js
@@ -0,0 +1,13 @@
+/* Theme customizations */
+$(document).ready(function() {
+if ( typeof SLB == 'undefined' || typeof SLB.View == 'undefined' || typeof SLB.View.extend_theme == 'undefined' )
+	return false;
+/**
+ * Extend theme
+ * @param string id Theme to extend
+ * @param object props Theme properties to add to theme 
+ */
+SLB.View.extend_theme('slb_theme_example', {
+	
+});
+});
