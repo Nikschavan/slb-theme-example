eae4e594350b4dd9ae7d29b1f2c452e2d56270e2
SM <sol@archetyped.com>
2013-10-17 08:32:37 -1000

    Init plugin

---
+++ main.php
@@ -0,0 +1,12 @@
+<?php 
+/* 
+Plugin Name: Simple Lightbox: Custom Theme (Example)
+Plugin URI: https://github.com/archetyped/slb-theme-example
+Description: Custom theme example for Simple Lightbox
+Version: 0.1
+Author: Archetyped
+Author URI: http://archetyped.com
+Support URI: https://github.com/archetyped/slb-theme-example/wiki/Reporting-Issues
+
+Copyright 2013 Sol Marchessault (sol@archetyped.com)
+*/
